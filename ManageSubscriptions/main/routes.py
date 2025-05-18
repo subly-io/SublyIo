5b878884f667a98f9ef2c8da71932ddfb20b87f6
subly-io <sublyiowebsite@gmail.com>
2025-05-18 06:44:03 +0300

    Update routes.py

--- ManageSubscriptions/main/routes.py
+++ ManageSubscriptions/main/routes.py
@@ -13,61 +13,6 @@ def index():
 @main_bp.route('/pricing', methods=["GET","POST"])
 @limiter.limit("5 per 10 minutes", methods=["POST"])
 def pricing():
-    if request.method == "POST":
-        try:
-            plan = request.form.get('plan')
-            if current_user.is_authenticated:
-                client = current_user
-                if plan =='pro':
-                    try:
-                        if client.subscription_type == 'pro':
-                            flash('Your pro plan has been renewed.','success')
-                        else:
-                            flash('You are now a pro user.','success')
-                        client.subscription_type = 'pro'
-                        client.remaining_days = 30
-                        client.allowed_requests = 20000
-                        client.subscribers_limit = 500
-                        client.plans_limit = 8
-                        client.notified_for_limit = False
-                        client.notified_for_expiration = False
-                        client.requests = 0
-                        db.session.commit()
-                        return redirect(url_for('dashboards.dashboard'))
-                    except:
-                        db.session.rollback()
-                        flash('Failed to complete your process, please try again later.','error')
-                        return redirect(url_for('dashboards.dashboard'))
-                elif plan =='ultimate':
-                    try:
-                        if client.subscription_type == 'ultimate':
-                            flash('Your ultimate plan has been renewed.','success')
-                        else:
-                            flash('You are now an ultimate user.','success')
-                        client.subscription_type = 'ultimate'
-                        client.remaining_days = 30
-                        client.allowed_requests = 200000
-                        client.subscribers_limit = 5000
-                        client.plans_limit = 15
-                        client.notified_for_limit = False
-                        client.notified_for_expiration = False
-                        client.requests = 0
-                        db.session.commit()
-                        return redirect(url_for('dashboards.dashboard'))
-                    except:
-                        db.session.rollback()
-                        flash('Failed to complete your process, please try again later.','error')
-                        return redirect(url_for('dashboards.dashboard'))
-                else:
-                    flash('Invalid plan name','error')
-                    return redirect(url_for('main.pricing'))
-            else:
-                flash('Please login first','info')
-                return redirect(url_for('auth.login'))
-        except:
-            flash('Invalid request.','error')
-            return redirect(url_for('main.pricing'))
-
     return render_template('pricing.html', title='Subly - Pricing')
 
 @main_bp.route('/about', methods=["GET"])
