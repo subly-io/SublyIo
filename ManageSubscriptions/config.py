6d4432af4beea43087f53aa0cb3bcafd62712a2a
subly-io <sublyiowebsite@gmail.com>
2025-05-18 06:49:43 +0300

    Update config.py

--- ManageSubscriptions/config.py
+++ ManageSubscriptions/config.py
@@ -1,13 +1,13 @@
 import os
 class Config:
-    SECRET_KEY = 'd387cdf8665539634137a282f049f3df476e462f51265c2742ece552ebfb35a9'
-    SQLALCHEMY_DATABASE_URI = "sqlite:///clients.db"
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     MAX_CONTENT_LENGTH = 5 * 1024 * 1024 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 587
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = os.environ.get('MAIL_PORT')
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'itsubly@gmail.com'
-    MAIL_PASSWORD = 'aeikwdyxnsozvvap'
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     SCHEDULER_API_ENABLED = True
     SCHEDULER_TIMEZONE = 'Asia/Amman'
